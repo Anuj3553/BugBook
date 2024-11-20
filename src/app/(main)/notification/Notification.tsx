ff61984c7a50edd8cdee7a863c871fcee9ef36bc
Anuj3553 <anujverma3553@gmail.com>
2024-11-20 16:10:21 +0530

    Fix notification link to use postId instead of username for follow notifications

--- src/app/(main)/notification/Notification.tsx
+++ src/app/(main)/notification/Notification.tsx
@@ -17,7 +17,7 @@ export default function Notification({ notification }: NotificationProps) {
         FOLLOW: { // The follow notification
             message: `${notification.issuer.displayName} followed you.`,
             icon: <User2 className="size-7 text-primary" />,
-            href: `/users/${notification.issuer.username}`,
+            href: `/users/${notification.postId}`,
         },
         COMMENT: { // The comment notification
             message: `${notification.issuer.displayName} commented on your post.`,
